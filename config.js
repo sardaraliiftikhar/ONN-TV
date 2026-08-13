513785ecc6715af6c9a2439fb5d0e9648582bb2d
sardaraliiftikhar <sardaraliiftikharsk@gmail.com>
2026-08-13 12:09:47 +0500

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // We will fill these two values after the GitHub/Supabase connection step.
 // Do NOT put a Supabase service_role/secret key here.
 
-const SUPABASE_URL = "";
-const SUPABASE_PUBLISHABLE_KEY = "";
+const SUPABASE_URL = "https://llthkgouliyrdbtuxorp.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_BK-4qs_-E6EJud9OwjP0RA_6jv6QvPj";
